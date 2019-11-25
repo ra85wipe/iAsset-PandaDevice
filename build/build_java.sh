6c07d213cc54ea60c6e27630ff3fb031d0539e30
Mathias Schmoigl <mathias.schmoigl@salzburgresearch.at>
2019-11-25 17:07:08 +0100

    updated java build file to use git submodule for basyx implementation

--- build/build_java.sh
+++ build/build_java.sh
@@ -6,21 +6,22 @@ echo "CWD: $CWD"
 
 # build basyx project for java sdk
 cd ../iAsset-Basyx/sdks/java/basys.sdk
-mvn clean install
+mvn clean install -DskipTests
 cd "$CWD"
 
 # build basyx project for components
 cd ../iAsset-Basyx/components/basys.components
-mvn clean install
+mvn clean install -DskipTests
 cd "$CWD"
 
 # build basyx project for examples (not needed)
 cd ../iAsset-Basyx/examples/basys.examples
-mvn clean install
+mvn clean install -DskipTests
 cd "$CWD"
 
 # build panda project of iAsset
 cd ../panda/basys.panda
+mvn clean install
 mvn verify
 cd "$CWD"
 
